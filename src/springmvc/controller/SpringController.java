96fb9f560172d6fc111b964c579ba5c7c2a0c234
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-25 09:10:20 -0800

    Create SpringController.java

---
+++ src/springmvc/controller/SpringController.java
@@ -0,0 +1,24 @@
+package springmvc.controller;
+
+import java.sql.Timestamp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import springmvc.utility.SqlTimestampPropertyEditor;
+
+@Controller
+public class SpringController {
+
+	/**
+	 * For handling non-primitive type data from HTML form submissions.
+	 * 
+	 * @param binder
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SqlTimestampPropertyEditor stpe = new SqlTimestampPropertyEditor("yyyy-MM-dd HH:mm:ss");
+	    binder.registerCustomEditor(Timestamp.class, stpe);
+	}
+}
